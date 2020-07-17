16efcb4a24026b475905b12deafcef5a1ab0a2f2
Harm Peter Millaard <68119941+hpmillaard@users.noreply.github.com>
2020-07-17 19:16:12 +0200

    Update import Drivers.ps1

--- import Drivers.ps1
+++ import Drivers.ps1
@@ -4,8 +4,3 @@ New-PSDrive -Name "DS001" -PSProvider MDTProvider -Root "D:\DeploymentShare"
 
 MD "DS001:\Out-of-Box Drivers\VMware"
 import-mdtdriver -path "DS001:\Out-of-Box Drivers\VMWare" -SourcePath "D:\MDT\Drivers\VMware" -Verbose
-MD "DS001:\Out-of-Box Drivers\Sony Vaio VGN-FW31ZJ"
-import-mdtdriver -path "DS001:\Out-of-Box Drivers\Sony Vaio VGN-FW31ZJ" -SourcePath "D:\MDT\Drivers\Sony Vaio VGN-FW31ZJ" -Verbose
-
-MD "DS001:\Out-of-Box Drivers\ASUS K50C"
-import-mdtdriver -path "DS001:\Out-of-Box Drivers\ASUS K50C" -SourcePath "D:\MDT\Drivers\ASUS K50C" -Verbose
