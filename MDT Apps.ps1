f1f4c38dcdaaa5c20f088ff87142084f63093d07
Harm Peter Millaard <68119941+hpmillaard@users.noreply.github.com>
2020-07-17 21:02:20 +0200

    Update MDT Apps.ps1

--- MDT Apps.ps1
+++ MDT Apps.ps1
@@ -1,5 +1,5 @@
 $DeploymentShare = "D:\DeploymentShare"
-$Source = "\\LT-5CG70220KT\Dropbox$\Software"
+$Source = "D:\MDT\Apps"
 $Import = $true
 
 If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit}
