a811dca48a27b543de3bf6035d2bc0a6cc3170f6
Harm Peter Millaard <68119941+hpmillaard@users.noreply.github.com>
2020-07-18 08:09:36 +0200

    Delete import Drivers.ps1

--- import Drivers.ps1
+++
@@ -1,6 +0,0 @@
-If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
-Import-Module "C:\Program Files\Microsoft Deployment Toolkit\bin\MicrosoftDeploymentToolkit.psd1"
-New-PSDrive -Name "DS001" -PSProvider MDTProvider -Root "D:\DeploymentShare"
-
-MD "DS001:\Out-of-Box Drivers\VMware"
-import-mdtdriver -path "DS001:\Out-of-Box Drivers\VMWare" -SourcePath "D:\MDT\Drivers\VMware" -Verbose
