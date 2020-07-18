d807a61ec88a2fd57cc3efc4165f3aef0f9abc46
Harm Peter Millaard <68119941+hpmillaard@users.noreply.github.com>
2020-07-18 07:06:09 +0200

    Update MDT Apps.ps1

--- MDT Apps.ps1
+++ MDT Apps.ps1
@@ -1,6 +1,6 @@
 $DeploymentShare = "D:\DeploymentShare"
 $Source = "D:\MDT\Apps"
-$Import = $true
+$Import = $false
 
 If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit}
 
